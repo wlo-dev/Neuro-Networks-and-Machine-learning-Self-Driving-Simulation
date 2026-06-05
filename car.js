a0c7798622ee3aed64306e0c83c4487fe5a0183a
wlo-dev <chagulukawillouby0@gmail.com>
2026-06-05 21:00:39 +0200

    Car direction fix

--- car.js
+++ car.js
@@ -9,6 +9,7 @@ class Car{
         this.acceleration=0.2;
         this.maxSpeed=3;
         this.friction=0.05;
+        this.angle=0;
 
         this.controls=new Controls();
     }
@@ -40,27 +41,40 @@ class Car{
         if(Math.abs(this.speed)<this.friction){
             this.speed=0;
 
-        }
-
+        } 
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
         if(this.controls.left){
-            this.x-=2;
+            this.angle+=0.03*flip;
         }
         if(this.controls.right){
-            this.x+=2;
+            this.angle-=0.03;
         }
 
-      this.y-=this.speed;
+    }  
+      
 
+
+
+      this.x-=Math.sin(this.angle)*this.speed;
+      this.y-=Math.cos(this.angle)*this.speed;
+  
     }
 
     draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+        
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
+        -this.width/2,
+        -this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+       ctx.restore(); 
     }
 }

34cd095eba15763a70ecb23c787e54091c16f126
wlo-dev <chagulukawillouby0@gmail.com>
2026-06-21 03:27:38 +0200

    completing lane

--- road.js
+++ road.js
@@ -27,16 +27,14 @@ class Road {
                 this.right,
                 i/this.laneCount
             );
+            ctx.beginPath();
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(x,this.bottom);
+            ctx.stroke();
         }
-        ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.bottom);
-        ctx.stroke();
-
-
-        ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
     }
+}
+
+function lerp(A,B,t){
+    return A+(B-A) *t;
 }

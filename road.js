9cb094e5ebb27cdf370fc8f02d07b9a15a02e738
wlo-dev <chagulukawillouby0@gmail.com>
2026-07-08 02:22:14 +0200

    fixing

--- road.js
+++ road.js
@@ -1,48 +1,43 @@
 class Road {
-    constructor (x,width, laneCount=4){
-        this.x=x;
-        this.width=width;
-        this.laneCount=laneCount;
+    constructor(x, width, laneCount = 4) {
+        this.x = x;
+        this.width = width;
+        this.laneCount = laneCount;
 
-        this.left=x-width/2;
-        this.right=x+width/2;
+        this.left = x - width / 2;
+        this.right = x + width / 2;
 
-
-
-        const infinity=1000000;
-        this.top=-infinity;
-        this.bottom=infinity;
-
+        const infinity = 1000000;
+        this.top = -infinity;
+        this.bottom = infinity;
     }
 
-getLaneCenter(laneIndex){
-    const laneWidth=this.width/this.laneCount;
-    return this.left+linewidth/2+laneIndex*lineWidth;
-}
+    getLaneCenter(laneIndex) {
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneWidth / 2 + Math.min(laneIndex, this.laneCount - 1) * laneWidth;
+    }
 
-   draw(ctx){
-    ctx.lineWidth=5;
-    ctx.strokeStyle="white";
+    draw(ctx) {
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "white";
 
-    for(let i=0;i<=this.laneCount;i++){
-        const x=lerp(
-            this.left,
-            this.right,
-            i/this.laneCount
-        );
+        for (let i = 0; i <= this.laneCount; i++) {
+            const x = lerp(
+                this.left,
+                this.right,
+                i / this.laneCount
+            );
 
-        if(i>0 && i<this.laneCount){
-            ctx.setLineDash([20,20]);
-        }else{r
-            ctx.setLineDash([]);
+            if (i > 0 && i < this.laneCount) {
+                ctx.setLineDash([20, 20]);
+            } else {
+                ctx.setLineDash([]);
+            }
+
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
         }
-
-        ctx.beginPath();
-        ctx.moveTo(x,this.top);
-        ctx.lineTo(x,this.bottom);
-        ctx.stroke();
     }
 }
-
-}
-
